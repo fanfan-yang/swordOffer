018155e8afd7a56defffd30685ea5dc27f1552a5
fanfan-yang <fany3@andrew.cmu.edu>
2019-10-02 17:33:10 -0700

    My answer

--- src/com/company/offer3_duplicate/Solution2.java
+++ src/com/company/offer3_duplicate/Solution2.java
@@ -3,12 +3,21 @@ package com.company.offer3_duplicate;
 /**
  * Do not make changes to the input array
  *
- * to find the duplicated numbers in the input array,
+ * to find one of the duplicated numbers in the input array,
  * whose length is n + 1, and all elements are within 1 ~ n
  *
  */
 
 public class Solution2 {
 
-    public static boolean findDuplication (int[] num, int[] )
+    public static int findDuplication (int[] num){
+
+        //Forbidden null input array and empty input array
+        if(num == null || num.length == 0){
+            return -1;
+        }
+
+        // no-empty input array
+        
+    }
 }
